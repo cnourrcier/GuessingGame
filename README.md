3d863293646d0f4dd23124def667ee8e7fde8dfa
Charlie Nourrcier <cnourrcier@hotmail.com>
2023-08-24 20:51:42 -0500

    Update to check for valid user input.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This is a simple numbers guessing game written in Python.
+
+
+8/24/23
+- Update to check for valid user input.
--- guessing_game.py
+++ guessing_game.py
@@ -7,16 +7,27 @@ class GuessingGame():
         self.max_attempts = 10
 
     def start(self):
-        print("Welcome to the Guessing Game! Guess a number between 1 and 100.")
+        print("Welcome to the Guessing Game!")
         self.play()
     
     def play(self):
         while self.attempts < self.max_attempts:
-            guess = int(input("Guess a number between 1 and 100: "))
+            guess = self.get_valid_guess()
             self.attempts += 1
             self.check_guess(guess)
         
         print("Game over! The number was", self.target_number,".")
+
+    def get_valid_guess(self):
+        while True:
+            try:
+                guess = int(input("Guess a number between 1 and 100: "))
+                if guess < 1 or guess > 100:
+                    print("Invalid number. Please try again.")
+                    continue
+                return guess
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
 
     def check_guess(self, guess):
         if guess < self.target_number:
